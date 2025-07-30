04bb57f7bba1f949b9357c73552b90f9b4905bfa
bdushime <bdushime47@gmail.com>
2025-07-30 21:30:32 +0200

    dev commit

--- travel-Journal/src/App.jsx
+++ travel-Journal/src/App.jsx
@@ -7,8 +7,12 @@ export default function App() {
     const entryElements = data.map((entry) => {
         return (
             <Entry
-                key={entry.id}
-                entry={entry}
+                img={entry.img}
+                title={entry.title}
+                country={entry.country}
+                googleMapsLink={entry.googleMapsLink}
+                dates={entry.dates}
+                text={entry.text}
             />
         )
     })
